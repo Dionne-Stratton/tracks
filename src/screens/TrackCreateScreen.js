af67ecc6e497dd9037bd1c6355558f530760c548
Dionne <mylife33@hotmail.com>
2022-05-14 20:55:11 -0500

    wip

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,14 +1,16 @@
 import '../_mockLocation'
-import React, {useEffect, useState} from 'react'
+import React, {useEffect, useState, useContext} from 'react'
 import { StyleSheet} from 'react-native'
 import {FontAwesome} from '@expo/vector-icons'
 import {Text} from 'react-native-elements'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { Accuracy, requestForegroundPermissionsAsync, watchPositionAsync } from 'expo-location'
 import Map from '../components/Map'
+import { Context as LocationContext } from '../context/LocationContext'
 
 
 const TrackCreateScreen = () => {
+    const { addLocation } = useContext(LocationContext)
     const [err, setErr] = useState(null)
 
     const startWatching = async () => {
@@ -22,7 +24,7 @@ const TrackCreateScreen = () => {
                   timeInterval: 1000,
                   distanceInterval: 10
               }, (location) => {
-                console.log(location)
+                addLocation(location)
               })
           }
         } catch (e) {
