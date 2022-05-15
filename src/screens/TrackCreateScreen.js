c5e76547a191a6e718114545aafbd7299d3a6c98
Dionne <mylife33@hotmail.com>
2022-05-14 19:56:52 -0500

    wip

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,16 +1,35 @@
-import React from 'react'
+import '../_mockLocation'
+import React, {useEffect, useState} from 'react'
 import { StyleSheet} from 'react-native'
 import {FontAwesome} from '@expo/vector-icons'
 import {Text} from 'react-native-elements'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { requestForegroundPermissionsAsync } from 'expo-location'
 import Map from '../components/Map'
 
 
 const TrackCreateScreen = () => {
+    const [err, setErr] = useState(null)
+
+    const startWatching = async () => {
+        try {
+          const { granted } = await requestForegroundPermissionsAsync();
+          if (!granted) {
+            throw new Error('Location permission not granted');
+          }
+        } catch (e) {
+          setErr(e);
+        }
+      };
+
+      useEffect(() => {
+          startWatching()
+      }, [])
     return (
         <SafeAreaView forceInset={{ top: 'always'}}>
             <Text h2>Create</Text>
             <Map />
+            {err ? <Text>Please allow tracking.</Text> : null}
         </SafeAreaView>
         )
 }
